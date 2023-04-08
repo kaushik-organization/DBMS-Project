103132c298435677ec71340beae1d778ca72e3a4
Kaushik Bhowmick <kaushiksbhowmick@gmail.com>
2023-04-08 18:31:43 +0530

    Made dashboard for viewing tables, made form for inserting a new book still some work to be done in that

---
+++ frontend/src/components/Genre.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import { useEffect } from "react";
+import axios from "axios";
+
+export default function Genre() {
+  const [data, setData] = useState([]);
+  const getGenres = async () => {
+    try {
+      const { data } = await axios.get(
+        `${import.meta.env.VITE_BACKEND_URL}/get-genre`
+      );
+      setData((prev) => [...data.data]);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getGenres();
+  }, []);
+
+  return (
+    <div className="p-1 w-full">
+      <table className="border border-white/30">
+        <thead>
+          <tr className="border border-white/30">
+            {data?.length != 0 &&
+              Object.keys(data[0])?.map((item, index) => (
+                <th
+                  key={index}
+                  className="border border-white/30 bg-[#222] p-2 px-6 min-w-[200px]"
+                >
+                  {item}
+                </th>
+              ))}
+          </tr>
+        </thead>
+        <tbody>
+          {data?.map((item, index) => (
+            <tr key={index} className="border border-white/30">
+              {Object.values(item)?.map((val, ind) => (
+                <td
+                  key={ind}
+                  className="border border-white/30 text-center min-w-[300px]"
+                >
+                  <input
+                    type={"text"}
+                    className="bg-transparent p-2 px-6 text-center w-full"
+                    disabled
+                    value={val.toString() ? val : "null"}
+                  />
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
--- frontend/src/pages/admin/dashboard.jsx
+++ frontend/src/pages/admin/dashboard.jsx
@@ -1,9 +1,20 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import AES from "crypto-js/aes";
 import CryptoJS from "crypto-js";
+import DashboardNavbar from "../../components/DashboardNavbar";
+import LeftComponents from "../../components/LeftComponents";
+import Author from "../../components/Author";
+import Books from "../../components/Books";
+import Publisher from "../../components/Publisher";
+import Genre from "../../components/Genre";
+import BooksAuthor from "../../components/BooksAuthor";
+import BooksGenre from "../../components/BooksGenre";
+import InsertBook from "../../components/InsertBook";
 
 export default function Dashboard() {
+  const [isVisible, setIsVisible] = useState(false);
+  const [active, setActive] = useState(1);
   const navigate = useNavigate();
   useEffect(() => {
     const sessionValue = sessionStorage.getItem("session");
@@ -17,5 +28,29 @@ export default function Dashboard() {
       navigate("/admin/login");
     }
   }, []);
-  return <div>Dashboard</div>;
+  return (
+    <>
+      {isVisible && <InsertBook setIsVisible={setIsVisible} />}
+      <div className="bg-black theme-font w-screen h-screen flex flex-col">
+        <DashboardNavbar />
+        <div className="flex flex-1">
+          <div className="w-[300px] border-r border-white/20 shrink-0">
+            <LeftComponents active={active} setActive={setActive} />
+          </div>
+          <div className="flex-1 text-white overflow-x-scroll scrollbar-thin scrollbar-thumb-gray-600 scrollbar-track-[#222]">
+            {
+              {
+                1: <Author />,
+                2: <Books setIsVisible={setIsVisible} />,
+                3: <Publisher />,
+                4: <Genre />,
+                5: <BooksAuthor />,
+                6: <BooksGenre />,
+              }[active]
+            }
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
