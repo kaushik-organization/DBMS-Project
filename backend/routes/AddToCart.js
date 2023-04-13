a6c95d3eb2afc013a167d170dc7462ac61304769
Rutvik Ranpariya <103186531+Rutvik-R@users.noreply.github.com>
2023-04-13 22:10:07 +0530

    Adding removeToCart

--- backend/routes/AddToCart.js
+++ backend/routes/AddToCart.js
@@ -9,16 +9,34 @@ router.post('/AddToCart' , async (req , res) => {
     if(!Available) return res.sendStatus(501);
     if(Available.Available < quantity) return res.status(409).send("Not Available");
 
-    // let [status] = await conn.query(`update Sales set quantity = quantity - ${quantity} , books_sold = books_sold + ${quantity} where book_id = '${book_id}'`); 
     const basket_id = user_id.replace('USER' , 'BASK');
 
     const [[{count}]] = await conn.query(`select count(book_id) as count from Basket_Books where book_id = '${book_id}' and basket_id='${basket_id}'`)
    
-    if(count) [status] = await conn.query(`update Basket_Books set count = count + ${quantity} where book_id = '${book_id}' and basket_id='${basket_id}'`); 
-    else [status] = await conn.query(`insert into Basket_Books values ('${basket_id}' , '${book_id}' , ${quantity})`)
+    if(count) await conn.query(`update Basket_Books set count = count + ${quantity} where book_id = '${book_id}' and basket_id='${basket_id}'`); 
+    else  await conn.query(`insert into Basket_Books values ('${basket_id}' , '${book_id}' , ${quantity})`)
     
     conn.end();
     res.sendStatus(200);
+})
+
+router.post('/removeToCart' , async (req , res) => {
+    const conn = await database.connectionStart();
+    const {book_id , user_id , quantity} = req.fields;
+    const basket_id = user_id.replace('USER' , 'BASK');
+    const [[Available]] = await conn.query(`select count as Available from Basket_Books where basket_id='${basket_id}' and book_id = '${book_id}'`);
+
+    if(!Available) return res.sendStatus(501);
+    if(Available.Available < quantity) return res.status(409).send("Not Available");
+
+    if(Available.Available == quantity) {
+        conn.query(`delete from Basket_Books where basket_id='${basket_id}' and book_id = '${book_id}'`)
+    }
+    else conn.query(`update Basket_Books set count = count - ${quantity} where book_id = '${book_id}' and basket_id='${basket_id}'`); 
+
+    conn.end();
+    res.sendStatus(200);
+    
 })
 
 module.exports = router;
