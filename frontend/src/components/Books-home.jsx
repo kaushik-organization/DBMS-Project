686e5e2fc4c0e707f3e17657fa8be06a71dbbbd9
Hrishikesh Makwana <hrishi1563@gmail.com>
2023-04-16 11:26:03 +0530

    home page

--- frontend/src/components/Books-home.jsx
+++ frontend/src/components/Books-home.jsx
@@ -14,8 +14,14 @@ export default function BooksHome() {
       <div className="flex flex-wrap h-[385px] overflow-hidden absolute -mt-8 justify-around m-auto gap-3 w-[80%] ">
         {books.map((item, index) => (
           <div className="bg-[#f4ecd5] border  px-2 py-2 pb-2  w-60 rounded-md">
-           <div  className="w-full h-[300px]"> <img src={item.image} alt=""  className=" object-top  object-cover w-full h-full rounded-md" />
-           </div>
+            <div className="w-full h-[300px]">
+              {" "}
+              <img
+                src={item.image}
+                alt=""
+                className=" object-top  object-cover w-full h-full rounded-md"
+              />
+            </div>
             <p className=" font-bold font-mono line-clamp-2 mt-2 text-center theme-font text-lg">
               {item.title}
             </p>
@@ -26,11 +32,10 @@ export default function BooksHome() {
         <h1 className="text-7xl text-white theme-font font-bold">
           There's no such
           <br />
-           thing as too many books
-           <br />
-           in your life
+          thing as too many books
+          <br />
+          in your life
         </h1>
-        
       </div>
     </div>
   );
--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -3,7 +3,12 @@ import React from "react";
 import { useEffect } from "react";
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import { AiOutlineShoppingCart } from "react-icons/ai";
+import {
+  AiFillAppstore,
+  AiOutlineShoppingCart,
+  AiOutlineTwitter,
+} from "react-icons/ai";
+import { FaStore } from "react-icons/fa";
 import Books from "../components/Books-home";
 
 export default function Home() {
@@ -25,6 +30,18 @@ export default function Home() {
       }
     });
   }, []);
+
+  const greeting = () => {
+    let today = new Date();
+    let curHr = today.getHours();
+    if (curHr < 12) {
+      return "morning";
+    } else if (curHr < 18) {
+      return "afternoon";
+    } else {
+      return "evening";
+    }
+  };
   // return (
   //   <div className="">
   //     {auth ? (
@@ -44,26 +61,69 @@ export default function Home() {
   // );
   return (
     <div className="w-full overflow-auto">
-      <div className="w-full h-[600px] overflow-hidden -z-10 ">
-        <div className="flex gap-6 w-full justify-end absolute p-4 px-10 py-6  bg-transparent text-black text-xl font-semibold theme-font">
-          <div>Shop</div>
-          <div>Blog</div>
-          <div>About Us</div>
-          <div>
-            <AiOutlineShoppingCart className="h-6 w-6" />
+      <div className="w-full h-[600px] overflow-hidden ">
+        <div className="flex gap-6 w-full justify-between items-center absolute p-4 px-10 py-6 text-black bg-black/10 z-10 text-xl font-semibold theme-font">
+          <div className="w-44">
+            <img
+              src="bookstore-logo/png/logo-no-background.png"
+              className="brightness-200"
+            />
+          </div>
+          <ul className="flex gap-6 text-black/80 items-center">
+            <li>
+              <AiFillAppstore className="w-8 h-8" />
+            </li>
+            <li>Store</li>
+            <li>Contact</li>
+            <li>
+              <AiOutlineTwitter className="w-8 h-8" />
+            </li>
+          </ul>
+          <div className="flex items-center gap-3">
+            {auth ? (
+              <>
+                <p className="text-center leading-3">
+                  <span className="text-neutral-500 text-sm">
+                    Good {greeting()},{" "}
+                  </span>
+                  <br />
+                  {name}
+                </p>
+                <AiOutlineShoppingCart className="w-8 h-8 text-blue-600" />
+                <div className="w-10 aspect-square rounded-full overflow-hidden flex justify-center items-center p-1 border border-neutral-500 cursor-pointer">
+                  <img src={photo} className="object-cover rounded-full" />
+                </div>
+              </>
+            ) : (
+              <>
+                <Link
+                  to="/auth0/login"
+                  className="bg-fuchsia-600 text-white shadow-2xl px-6 p-1 rounded-sm font-semibold mt-2"
+                >
+                  Sign In
+                </Link>
+              </>
+            )}
           </div>
         </div>
-        <div className="flex w-full h-full bg-[#f9edd8] pb-10">
-          <div className="w-1/2 flex items-center px-20 shrink-0 font-extrabold text-7xl theme-font">
-            A place where <br />
-            world collides
+        <div className="flex w-full h-full bg-[#fbdead] pb-10">
+          <div className="w-1/2 flex flex-col gap-10 justify-center mt-20 pl-14 shrink-0 font-extrabold relative text-7xl theme-font">
+            <div className="w-60 h-60 ml-[1000px] border border-black z-10 absolute header-gradient-color" />
+            <div className="w-60 h-60 mt-40 ml-80 border border-black z-10 absolute header-gradient-color" />
+            <p className="relative">
+              <span className="text-fuchsia-600">A place</span> where <br />
+              world unite to collide
+            </p>
+            <button className="bg-fuchsia-600 px-20 p-4 text-white flex items-center gap-3 flex-row-reverse text-semibold text-2xl font-semibold w-fit rounded-sm">
+              Check out the store <FaStore className="w-6 h-6" />
+            </button>
           </div>
           <div className="w-1/2 h-full">
-          <img
-            src="123456.png"
-            alt=""
-            className="shrink-0 object-contain w-full h-full scale-110"
-          />
+            <img
+              src="123456.png"
+              alt=""
+              className="shrink-0 object-contain w-full h-full scale-110"
+            />
           </div>
         </div>
       </div>
